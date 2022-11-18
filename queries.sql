a553f102ce2aa0ff239ee6dadcbe35823ebf6b19
Your Name <you@example.com>
2022-11-17 19:22:36 -0600

    did SQL tests

--- queries.sql
+++ queries.sql
@@ -8,30 +8,21 @@
 -- SQL TASK: In queries.sql under “Part 2”, write a query to list the names of the employers in St. Louis City.
 --Do NOT specify an ordering for the query results.
 
-SELECT location
-FROM techjobs
-WHERE (attending = 1) AND (employer = "St. Louis City");
+SELECT name
+FROM employer
+WHERE location = "ST. Louis City";
 
 -- Part 3: Test it with SQL
 -- SQL TASK: In queries.sql under “Part 3”, write the SQL statement to remove the job table.
 
-DELETE TABLE job (
-   id int,
-   name varchar(100),
-   employerId int
-);
-
-SELECT *
-FROM techjobs
-ORDER BY id
-DELETE FROM techjobs
-ROLLBACK;
+DROP TABLE job;
 
 -- Part 4: Test it with SQL
 --  SQL TASK: In queries.sql under “Part 4”, write a query to return the names of all skills that are attached to jobs in alphabetical order.
 -- If a skill does not have a job listed, it should not be included in the results of this query.
 
 SELECT *
-FROM techjobs AS
-JOIN  AS
-ON
+FROM skill
+LEFT JOIN job_skills ON skill.id=job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
